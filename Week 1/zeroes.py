f8c28eb43eeee14b32e1f6bbca456f2bb66c8910
Jaseemck <jaseemckclt@gmail.com>
2020-05-03 18:01:05 +0530

    Added move zeroes

---
+++ Week 1/zeroes.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(0,len(nums)):
+            if nums[i]==0:
+                nums.append(nums.pop(i))
+n=[0,1,0,3,12]
+ans = Solution()
+ans.moveZeroes(n)
+print(n)
